ecf53219dec8cb586aba8a036e46063792c098a6
vishwachandra2710 <vishwachandra2710@gmail.com>
2024-07-30 21:22:18 +0530

    Create ipadress.py

---
+++ ipadress.py
@@ -0,0 +1,24 @@
+'''
+Python Program to Find IP Address
+'''
+
+# Import the necessary module!
+import socket as s 
+
+# Get my hostname
+my_hostname = s.gethostname()
+# Display my hostname
+print("Your Hostname is: " + my_hostname)
+
+# Get my IP
+my_ip = s.gethostbyname(my_hostname)
+# Display my IP 
+print("Your Ip Address is: " + my_ip)
+
+# Set the hostname
+host = "github.com"
+# Fetch the IP
+ip = s.gethostbyname(host)
+
+# Display the IP
+print("The IP Address of " + host + " is: "  + ip)
